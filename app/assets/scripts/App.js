b101c757110e80f87729f602259eaeecae583ca1
iawasthi <resonanceoncl@gmail.com>
2018-02-18 22:48:17 -0600

    Completed Sticky Header

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll'; 
+import StickyHeader from './modules/StickyHeader';
  
 var mobileMenu = new MobileMenu();
-var revealFeatures = new RevealOnScroll( $(".feature-item"), "85%");
-//var revealTestimonials = new RevealOnScroll();
+var revealOnScroll = new RevealOnScroll( $(".feature-item"), "85%");
+var revealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
